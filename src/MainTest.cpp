4b557d42a2af9db26392bc6a5ad77f72ed1faaa8
vicent_zeng <891605243@qq.com>
2021-01-02 00:20:12 +0800

    [ISP simulation] convert raw10 to raw8, to temp support raw10 temp

--- src/MainTest.cpp
+++ src/MainTest.cpp
@@ -92,40 +92,56 @@ uint32_t writeRGB2Png(uint8_t * buf, const char * path,
     return 0;
 }
 
-int main() {
-    // Read input raw
+int runIspPipeline(uint8_t* rawBuf, int width, int height, int raw_bits, int buf_size) {
     int ret = 0;
     char filePath[256] = {0};
-    uint32_t read_out_cnt = 0;
     uint32_t write_in_cnt = 0;
-    uint8_t* rawBuf = readFile2Buf( "../../test_raw/raw_4208x3120_8bits.raw", read_out_cnt);
-    if( rawBuf == 0) {
-        LOGE("readFile2Buf failed!");
-        return 0;
-    }
-    LOGI("%s read:%ld\n", __func__, read_out_cnt);
-
+    uint16_t* new16bitsRaw = nullptr;
+    uint8_t* new8bitsRaw = nullptr;
+    size_t raw_pixel_size = width * height;
     //sprintf(filePath, "%s/%s", OUTPUT_PATH, "isp_s0_raw_4208x3120_8bits.raw");
     //writeFile(rawBuf, "../test_raw/output/raw_4208x3120_8bits_unpack_out.raw", read_out_cnt);
     ISPPipelineIntf ispIntf;
     LOGI("ISP pipeline process start");
 
-    // Turn to 16bits
-    LOGI("Turn to 16bits E");
-    uint16_t* new16bitsRaw = (uint16_t*)malloc(read_out_cnt * 2);
-    for (uint32_t i = 0; i < read_out_cnt; i++) {
-        *(new16bitsRaw + i) = *(rawBuf + i);
+    // Raw Buf prepare
+    uint8_t* pipelineRawBuf = rawBuf;
+    if (8 == raw_bits) {
+        // Turn to 16bits, No use code
+        LOGI("Turn to 16bits E");
+        new16bitsRaw = (uint16_t*)malloc(raw_pixel_size * 2);
+        for (uint32_t i = 0; i < raw_pixel_size; i++) {
+            *(new16bitsRaw + i) = *(rawBuf + i);
+        }
+        write_in_cnt = raw_pixel_size * 2;
+        writeFile((uint8_t*)new16bitsRaw, OUTPUT_PATH, "PlaneTo16_4208x3120_Turn16bits_out.raw", write_in_cnt);
+    } else if (10 == raw_bits) {
+        if (buf_size != raw_pixel_size * 2) {
+            LOGE("raw buf size err!");
+            return -EINVAL;
+        }
+        LOGI("Turn to 16bits E");
+        uint16_t* temp_buf = (uint16_t*)rawBuf;
+        for (uint32_t i = 0; i < raw_pixel_size; i++) {
+            *(temp_buf + i) = *(temp_buf + i)>>2;   //raw10 >> 2 -> raw8
+        }
+        LOGI("Turn to 8bits E");    //now only support raw8
+        new8bitsRaw = (uint8_t*)malloc(raw_pixel_size);
+        for (uint32_t i = 0; i < raw_pixel_size; i++) {
+            *(new8bitsRaw + i) = (uint8_t)(*(temp_buf + i));   //raw10 >> 2 -> raw8
+        }
+
+        // replace pipelineRawBuf
+        pipelineRawBuf = new8bitsRaw;
     }
-    write_in_cnt = read_out_cnt * 2;
-    writeFile((uint8_t*)new16bitsRaw, OUTPUT_PATH, "PlaneTo16_4208x3120_Turn16bits_out.raw", write_in_cnt);
 
     // Demosaic
     LOGI("Demosaic E");
     ImgFrame rawFrame;
-    rawFrame.imgBuf = rawBuf;
-    rawFrame.width = 4208;
-    rawFrame.height = 3120;
-    rawFrame.bits = 8;
+    rawFrame.imgBuf = pipelineRawBuf;
+    rawFrame.width = width;
+    rawFrame.height = height;
+    rawFrame.bits = 8;  //focus to 8 bits
     rawFrame.type = IMAGE_RAW_GRBG;
     rawFrame.isPacked = false;
     ImgFrame demosaicOut = rawFrame;
@@ -137,7 +153,7 @@ int main() {
         LOGE("demsaic failed!");
     }
 
-    write_in_cnt = read_out_cnt * 3;
+    write_in_cnt = raw_pixel_size * 3;    //for RGB
     //writeFile((uint8_t*)demosaicOut.imgBuf, OUTPUT_PATH, "Demosaic_4208x3120_8bits_out.rgb", write_in_cnt);
     writeRGB2Png((uint8_t*)demosaicOut.imgBuf, OUTPUT_PATH, "Demosaic_4208x3120_8bits_out.png",
         rawFrame.width, rawFrame.height);
@@ -176,11 +192,46 @@ int main() {
     // Release resource
     free(rawBuf);
     rawBuf = nullptr;
-    free(new16bitsRaw);
-    new16bitsRaw = nullptr;
+    if (new16bitsRaw != nullptr) {
+        free(new16bitsRaw);
+        new16bitsRaw = nullptr;
+    }
+
+    if (new8bitsRaw != nullptr) {
+        free(new8bitsRaw);
+        new8bitsRaw = nullptr;
+    }
+
     delete [] demosaicOut.imgBuf;
     demosaicOut.imgBuf = nullptr;
-    //
+    return 0;
+}
+
+int main() {
+    // Read input raw
+    int ret = 0;
+    char filePath[256] = {0};
+    uint32_t read_out_cnt = 0;
+    uint32_t write_in_cnt = 0;
+
+    // test1
+    uint8_t* rawBuf = readFile2Buf( "../../test_raw/raw_4208x3120_8bits.raw", read_out_cnt);
+    if( rawBuf == 0) {
+        LOGE("readFile2Buf failed!");
+        return 0;
+    }
+    LOGI("%s read:%ld\n", __func__, read_out_cnt);
+    //runIspPipeline(rawBuf, 4208, 3120, 8, read_out_cnt);
+
+    //test2
+    uint8_t* rawBuf2 = readFile2Buf( "../../test_raw/test2__4640x3488_10_2.raw", read_out_cnt);
+    if( rawBuf2 == 0) {
+        LOGE("readFile2Buf failed!");
+        return 0;
+    }
+    LOGI("%s read:%ld\n", __func__, read_out_cnt);
+    runIspPipeline(rawBuf2, 4640, 3488, 10, read_out_cnt);
+
     LOGD("openCV test E");
     Mat img = imread("main.jpg");
     imshow("image",img);
